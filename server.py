3bbcf46b32c06f4b025385bea4053f5758ed3282
Nobime Romeo <nobimeromeo@pop-os.localdomain>
2022-12-02 08:27:01 +0100

    first commit

---
+++ server.py
@@ -0,0 +1,83 @@
+import json
+from flask import Flask,render_template,request,redirect,flash,url_for
+
+def loadClubs():
+    with open('clubs.json') as c:
+         listOfClubs = json.load(c)['clubs']
+         return listOfClubs
+
+
+def loadCompetitions():
+    with open('competitions.json') as comps:
+         listOfCompetitions = json.load(comps)['competitions']
+         return listOfCompetitions
+
+
+app = Flask(__name__)
+app.secret_key = 'something_special'
+
+competitions = loadCompetitions()
+clubs = loadClubs()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/showSummary',methods=['POST'])
+def showSummary():
+    club = [club for club in clubs if club['email'] == request.form['email']][0]
+    return render_template('welcome.html',club=club,competitions=competitions)
+
+
+@app.route('/book/<competition>/<club>')
+def book(competition,club):
+    foundClub = [c for c in clubs if c['name'] == club][0]
+    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+    if foundClub and foundCompetition:
+        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+    else:
+        flash("Something went wrong-please try again")
+        return render_template('welcome.html', club=club, competitions=competitions)
+
+
+@app.route('/purchasePlaces',methods=['POST'])
+def purchasePlaces():
+    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+    club = [c for c in clubs if c['name'] == request.form['club']][0]
+    competition_name = str(competition['name'])
+    places_booked = str(competition['name'] + '_places_booked').replace(' ','_').lower()
+    places_required = int(request.form['places'])
+    number_of_places = int(competition['numberOfPlaces'])
+    points = int(club['points'])
+    MAX_PLACES_REQUIRED = 12
+
+    if not places_booked in club:
+            club[places_booked] = 0
+
+    if club[places_booked] + places_required > MAX_PLACES_REQUIRED:
+        flash("Sorry, you can't book more than 12 places for this competition.")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    elif places_required > number_of_places:
+        flash("Sorry, you have selected more places than available.")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    elif points < places_required:
+        flash("Sorry, you don't have enough points.")
+        return render_template('welcome.html', club=club, competitions=competitions)
+    else:
+        club[places_booked] += places_required
+        club['points'] = str(points-places_required)
+        competition['numberOfPlaces'] = number_of_places-places_required
+        flash(f'You have successfully booked {places_required} places for the {competition_name}')
+        return render_template('welcome.html', club=club, competitions=competitions)
+
+# TODO: Add route for points display
+@app.route('/showSummary',methods=['GET'])
+def showPoints():
+    return render_template('index.html', clubs=clubs)
+
+
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
